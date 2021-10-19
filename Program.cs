59de92fc0491bd9093e3c30cfd341662763a2ca1
jpeterseni35 <jpeterseni35@gmail.com>
2021-10-19 18:09:52 -0500

    C# Tutorial

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearnC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

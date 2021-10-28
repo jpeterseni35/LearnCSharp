b90a02625b5f58968b8e2a045587027011085133
jpeterseni35 <jpeterseni35@gmail.com>
2021-10-28 17:51:00 -0500

    Types AngelSix

---
+++ AngelSix/Types/Types/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myFloat;
+
+            myFloat = 112451.05f;
+            System.Console.WriteLine(myFloat);
+
+            bool myBool = true;
+            char myChar;
+            string myString;
+
+            System.Console.WriteLine($"A bool is {sizeof(bool)} bytes");
+
+            DateTime myDate;
+
+            // can represent a span of time (like 1 hour)
+            TimeSpan myTime;
+
+
+        }
+    }
+}

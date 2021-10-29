af44319f9e8432e714bf2f82e569633ae3668bed
jpeterseni35 <jpeterseni35@gmail.com>
2021-10-29 18:46:18 -0500

    Methods commit

---
+++ AngelSix/Methods/Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        // static defaults the method to private
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            System.Console.WriteLine(GetUserDateOfBirth("Bug"));
+            System.Console.WriteLine(GetUserDateOfBirth("Amadeus"));
+        }
+
+        public static string GetUserDateOfBirth(string fullName)
+        {
+            return fullName + " was born on Tuesday";
+        }
+    }
+}

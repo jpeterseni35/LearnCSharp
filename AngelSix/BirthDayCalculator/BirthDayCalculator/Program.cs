b96f1c3c1b74c371fda433fe40e8536bbd06156f
jpeterseni35 <jpeterseni35@gmail.com>
2021-11-01 17:44:52 -0500

    finished product

--- AngelSix/BirthDayCalculator/BirthDayCalculator/Program.cs
+++ AngelSix/BirthDayCalculator/BirthDayCalculator/Program.cs
@@ -7,45 +7,108 @@ namespace BirthDayCalculator
     {
         static void Main(string[] args)
         {
-
+            // Introduce our Savvy robot
             IntroduceSavvy();
 
+            // Ask the user for their date of birth
             var usersDateOfBirth = AskForDateOfBirth();
+
+            // Announce information about users date of birth
+            AnnounceDateOfBirthInformation(usersDateOfBirth);
+
         }
 
-
+        /// <summary>
+        /// Introduce Savvy the robot to our user
+        /// </summary>
         public static void IntroduceSavvy()
         {
+            // Output message to user
             System.Console.WriteLine("Hi, I'm Savvy. " +
                     "I'm really good at guessing the " +
                     "day of the week you were born on.");
         }
 
+        /// <summary>
+        /// Ask the user for their date of birth, until it is given in a correct format.
+        /// </summary>
+        /// <remarks>This call will not return until a valid date is entered or the application is terminated.</remarks>
+        /// <returns>The date of birth the user entered.</returns>
         public static DateTimeOffset AskForDateOfBirth()
         {
-            // string interpolation
-            System.Console.WriteLine($"Can I start by asking you " + $"what your date of birth is? {CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern}");
+            // Create checked date with initial out of range value
+            var dateOfBirth = DateTimeOffset.MaxValue;
 
-            var userText = Console.ReadLine();
-
-            var parsedDate = DateTimeOffset.MinValue;
-
-            var succeeded = DateTimeOffset.TryParse(userText, out parsedDate);
-
-            if (succeeded)
+            // Until the checked date of birth is greater than today...
+            while (dateOfBirth > DateTimeOffset.UtcNow)
             {
-                return parsedDate;
+                // Tell the user to enter their date of birth
+                System.Console.WriteLine($"Can I start by asking you " +
+                        $"what your date of birth is? {CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern}");
+
+                // Read their response
+                var userText = Console.ReadLine();
+
+                // If the user entered a valid date...
+                if (DateTimeOffset.TryParse(userText, out var parsedDate))
+                    // Set the checked date to users given date
+                    dateOfBirth = parsedDate;
+                // Otherwise...
+                else
+                    // Inform user they entered invalid date
+                    System.Console.WriteLine("You did not enter a valid date!");
+
             }
-            else
-            {
-                System.Console.WriteLine("You did not enter a valid date!");
 
-                return AskForDateOfBirth();
-            }
-
-
+            // Return the result
+            return dateOfBirth;
 
         }
 
+        /// <summary>
+        /// Display information to the user about the given date of birth.
+        /// </summary>
+        /// <param name="date">The date of birth to display information about.</param>
+        public static void AnnounceDateOfBirthInformation(DateTimeOffset date)
+        {
+            // Store current time
+            var now = DateTimeOffset.UtcNow;
+            // Display their date of birth.
+            System.Console.WriteLine($"So you were born on {date:d MMMM yyyy}.");
+
+            // Display day of the week they were born.
+            System.Console.WriteLine($"That was a {date.DayOfWeek}.");
+
+            //Indicate if we have already passed the users date of birth for the current year.
+            var hasPassedBirthday = now.DayOfYear > date.DayOfYear;
+
+            // Get a date representing the users next birthday.
+            var nextBirthday = new DateTimeOffset(now.Year + (hasPassedBirthday ? 1 : 0), date.Month, date.Day, 0, 0, 0, TimeSpan.Zero);
+
+            // Display how many days until the next birthday.           
+            System.Console.WriteLine($"It is {(nextBirthday - now).TotalDays:0} days until your next birthday.");
+
+            // Get the users age.
+            var userAge = now.Year - date.Year - (hasPassedBirthday ? 0 : 1);
+
+            // Display users age.
+            System.Console.WriteLine($"You are {userAge} years old.");
+
+            // Display users age in dog years.
+            System.Console.WriteLine($"You are {userAge * 7} years old in dog years.");
+
+            // Get users time alive.
+            var timeAlive = now - date;
+
+            // Display days old.
+            System.Console.WriteLine($"You are {timeAlive.TotalDays:n0} days old");
+
+            // Display hours old.
+            System.Console.WriteLine($"You are {timeAlive.TotalHours:n0} hours old");
+
+            // Display seconds old.
+            System.Console.WriteLine($"You are {timeAlive.TotalSeconds:n0} seconds old");
+        }
     }
+
 }
